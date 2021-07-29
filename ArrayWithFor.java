dfa67f78e770d4aabe6921764cf6f0f8805769d2
cesckyu <83571281+cesckyu@users.noreply.github.com>
2021-07-29 14:43:08 +0900

    v1
    기본 골격 생성

---
+++ ArrayWithFor.java
@@ -0,0 +1,10 @@
+public class ArrayWithFor{
+
+    public static void main(String[] args) {
+        int[] iarray = new int[100];
+        iarray[0] = 1;
+        iarray[1] = 2;
+        
+    }
+
+}
